1717d3c42181fe13fe9b71bc7845172f7da0cf09
yassin <makrai.yassin@gmail.com>
2020-10-20 13:41:25 +0200

    exo 6 terminé

--- 6-classes/6.2.js
+++ 6-classes/6.2.js
@@ -6,23 +6,16 @@ class rectangle{
         this.length=length;
     }
     collides(otherRectangle){
-        if( (this.topLeftXPos+this.width>= otherRectangle.topLeftXPos
+        if(  this.topLeftXPos+this.width>= otherRectangle.topLeftXPos
              && this.topLeftYPos+this.length>=otherRectangle.topLeftYPos
              && this.topLeftXPos <= otherRectangle.topLeftXPos
-             && this.topLeftYPos <= otherRectangle.topLeftYPos)
-             || (otherRectangle.topLeftXPos+otherRectangle.width>= this.topLeftXPos
+             && this.topLeftYPos <= otherRectangle.topLeftYPos
+             || otherRectangle.topLeftXPos+otherRectangle.width>= this.topLeftXPos
              && otherRectangle.topLeftYPos+otherRectangle.length>=this.topLeftYPos
              && otherRectangle.topLeftYPos <= this.topLeftYPos
-             && otherRectangle.topLeftXPos <= this.topLeftXPos)){
+             && otherRectangle.topLeftXPos <= this.topLeftXPos
+             ){
 
-        // if ((otherRectangle.topLeftXPos>=this.topLeftXPos && 
-        //     otherRectangle.topLeftXPos<=this.topLeftXPos+this.width && 
-        //     otherRectangle.topLeftYPos>=this.topLeftYPos && 
-        //     otherRectangle.topLeftYPos<=this.topLeftYPos+this.length)||
-        //     (this.topLeftXPos>=otherRectangle.topLeftXPos && 
-        //     this.topLeftXPos<=otherRectangle.topLeftXPos+otherRectangle.width && 
-        //     this.topLeftYPos>=otherRectangle.topLeftYPos && 
-        //     this.topLeftYPos<=otherRectangle.topLeftYPos+otherRectangle.length)) {
         return true
         } else{
             return false
@@ -30,27 +23,27 @@ class rectangle{
     }
 }
 
-let MyRectangle =   new rectangle (10,10,5,5);
-let YourRectangle = new rectangle (4,4,5,5);//false
-let rectangle1 = new rectangle (15,15,5,5);//true
-let rectangle2 = new rectangle (25,25,5,5);//false
-let rectangle3 = new rectangle (10,10,5,5);//true
-let rectangle4 = new rectangle (16,15,5,5);//false
-let rectangle5 = new rectangle (15,16,5,5);//false
-let rectangle6 = new rectangle (-10,10,5,5);//false
-let rectangle7 = new rectangle (5,6,5,5);//false
+// let MyRectangle =   new rectangle (10,10,5,5);
+let YourRectangle = new rectangle (6,7,5,5);
+// let rectangle1 = new rectangle (15,15,5,5);//true
+// let rectangle2 = new rectangle (25,25,5,5);//false
+// let rectangle3 = new rectangle (10,10,5,5);//true
+// let rectangle4 = new rectangle (16,15,5,5);//false
+// let rectangle5 = new rectangle (15,16,5,5);//false
+// let rectangle6 = new rectangle (-10,10,5,5);//false
+// let rectangle7 = new rectangle (4,5,5,5);//true
 
 
 
 console.log(MyRectangle);
 console.log(YourRectangle);
 console.log("collide: "+MyRectangle.collides(YourRectangle));
-console.log("collide: "+MyRectangle.collides(rectangle1));
-console.log("collide: "+MyRectangle.collides(rectangle2));
-console.log("collide: "+MyRectangle.collides(rectangle3));
-console.log("collide: "+MyRectangle.collides(rectangle4));
-console.log("collide: "+MyRectangle.collides(rectangle5));
-console.log("collide: "+MyRectangle.collides(rectangle6));
-console.log("collide: "+MyRectangle.collides(rectangle7));
+// console.log("collide: "+MyRectangle.collides(rectangle1));
+// console.log("collide: "+MyRectangle.collides(rectangle2));
+// console.log("collide: "+MyRectangle.collides(rectangle3));
+// console.log("collide: "+MyRectangle.collides(rectangle4));
+// console.log("collide: "+MyRectangle.collides(rectangle5));
+// console.log("collide: "+MyRectangle.collides(rectangle6));
+// console.log("collide: "+MyRectangle.collides(rectangle7));
 
 
--- 6-classes/6.3.js
+++ 6-classes/6.3.js
@@ -1,22 +1,58 @@
+// GENERER UN CHIFFRE RANDOM ENTRE 1 & 10
+
+function rand10(){ 
+    return Math.floor(Math.random()*100);
+}
+
+// CREE UN ARRAY DE 4 CHIFFRES RANDOM ENTRE 1 & 10
+
+function multiRand(){
+    let arr = [];
+    for(i=0; i<4; i++){
+        let x = rand10();
+        arr[i] = x;
+    }
+	return arr;
+}
+
+let random;
+
+
 class rectangle{
     constructor(topLeftXPos,topLeftYPos, width, length){
         this.topLeftXPos=topLeftXPos;
         this.topLeftYPos=topLeftYPos;
         this.width=width;
         this.length=length;
-    }
+	}	
     collides(otherRectangle){
-        if( (this.topLeftXPos+this.width>= otherRectangle.topLeftXPos && this.topLeftYPos+this.length>=otherRectangle.topLeftYPos)
-            ||(otherRectangle.topLeftXPos+otherRectangle.width>= this.topLeftXPos && otherRectangle.topLeftYPos+otherRectangle.length>=this.topLeftYPos)
-            ){
-            return true
+        if(  this.topLeftXPos+this.width>= otherRectangle.topLeftXPos
+             && this.topLeftYPos+this.length>=otherRectangle.topLeftYPos
+             && otherRectangle.topLeftXPos+otherRectangle.width>= this.topLeftXPos
+             && otherRectangle.topLeftYPos+otherRectangle.length>=this.topLeftYPos
+             ){
+
+        return true
         } else{
             return false
         }
     }
 }
 
-let MyRectangle = new rectangle ()
+let otherRectangle=[];
+
+for(let i = 0; i<10 ; i++){
+	random = multiRand();
+	otherRectangle[i] = new rectangle (random[0],random[1],random[2],random[3]);
+	console.log(otherRectangle);
+	for (let j=1; j<=i;j++){
+        console.log(otherRectangle[i].collides(otherRectangle[i-j]));
+    }
+}
+
+// random = multiRand();
+// let MyRectangle = new rectangle (random[0],random[1],random[2],random[3]);
+// console.log(MyRectangle);
 
 
-console.log("collide: "+Rectangle.collides());
+
